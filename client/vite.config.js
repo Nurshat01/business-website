176d07193060f17797288c0775f44a8a9ce8b215
unknown <sixstrings22@outlook.com>
2024-03-09 01:01:12 -0600

    Set up connection.js, added basic setup to start server with database, refactored to use Apollo-client, added bcrypt as dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,38 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+// import path from 'path';
 
+// export default defineConfig({
+//   root: path.resolve(__dirname, '..'), 
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001', 
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, '..'), 
+//     },
+//   },
+// });
+
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname, '..'), 
-  server: {
+  plugins: [react()],
+    server: {
+    port: 3000,
+    open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001', 
+      '/graphql': {
+        target: 'http://localhost:3001',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
       },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '..'), 
-    },
-  },
-});
+    }
+  }
+})
