a37a72082873740de51b76cd294f48226ccdee80
unknown <sixstrings22@outlook.com>
2024-03-11 20:05:24 -0500

    Added login and createUser functionality as well as JWT authentication

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,37 +1,39 @@
-// const { GraphQLError } = require('graphql');
-// const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-// const secret = process.env.JWT_SECRET;
-// const expiration = '2h';
+const secret = process.env.JWT_SECRET;
+const expiration = '2h';
 
-// module.exports = {
-//   AuthenticationError: new GraphQLError('Could not authenticate user.', {
-//     extensions: {
-//       code: 'UNAUTHENTICATED',
-//     },
-//   }),
-//   authMiddleware: function ({ req }) {
-//     let token = req.body.token || req.query.token || req.headers.authorization;
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
 
-//     if (req.headers.authorization) {
-//       token = token.split(' ').pop().trim();
-//     }
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-//     if (!token) {
-//       return req;
-//     }
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-//     try {
-//       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-//       req.user = data;
-//     } catch {
-//       console.log('Invalid token');
-//     }
+    if (!token) {
+      return req;
+    }
 
-//     return req;
-//   },
-//   signToken: function ({ email, username, _id }) {
-//     const payload = { email, username, _id };
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   },
-// };
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
