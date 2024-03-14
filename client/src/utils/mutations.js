2f90395ab35c57fb65d2cf3a37b90b6a7f622762
Deontae Hicks <72946905+Dhicks-dev@users.noreply.github.com>
2024-03-13 21:13:37 -0500

    updated tydefs resolvers

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,15 @@
-// mutations.js 
+// mutations.js
+import {gql} from '@apollo/client'
+export const Mutation_USERS = gql `{users { _id username email password}}`
+// add user
+export const ADD_USER = gql`
+ mutation addUser($email: String!, $password: String!) {
+   addUser(email: $email, password: $password) {
+     token
+     user {
+       _id
+       username
+     }
+   }
+ }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,3 @@
-// queries.js 
+// queries.js
+import { gql } from "@apollo/client";
+export const QUERY_USERS = gql`{ users { _id username email password} }`
