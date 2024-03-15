41be37c17b9f33434a243ec49fc7980547c52879
Adam <nuradam3033@gmail.com>
2024-03-14 19:35:23 -0500

    Refactor

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -1,54 +1,56 @@
 import React from 'react';
-import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-3 col-md-6">
-            <h4>Contact Us</h4>
+        <div className="columns">
+          <div className="column">
+            <h4 className="title is-4">Contact Us</h4>
             <p>123 Main Street</p>
             <p>City, State, ZIP</p>
             <p>Email: info@example.com</p>
             <p>Phone: 123-456-7890</p>
           </div>
-          <div className="col-lg-3 col-md-6">
-            <h4>Links</h4>
-            <ul className="list-unstyled">
+          <div className="column">
+            <h4 className="title is-4">Links</h4>
+            <ul>
               <li><a href="#">Home</a></li>
               <li><a href="#">About Us</a></li>
               <li><a href="#">Services</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
           </div>
-          <div className="col-lg-3 col-md-6">
-            <h4>Newsletter</h4>
+          <div className="column">
+            <h4 className="title is-4">Newsletter</h4>
             <p>Subscribe to our newsletter for the latest updates.</p>
-            <form>
-              <div className="form-group">
-                <input type="email" className="form-control" placeholder="Email Address" />
+            <form className="newsletter-form">
+              <div className="field is-grouped">
+                <div className="control is-expanded">
+                  <input type="email" className="input" placeholder="Email Address" />
+                </div>
+                <div className="control">
+                  <button type="submit" className="button is-primary">Subscribe</button>
+                </div>
               </div>
-              <button type="submit" className="btn btn-primary">Subscribe</button>
             </form>
           </div>
-          <div className="col-lg-3 col-md-6">
-            <h4>Follow Us</h4>
-            <ul className="list-unstyled">
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
-              <li><a href="#">Instagram</a></li>
+          <div className="column">
+            <h4 className="title is-4">Follow Us</h4>
+            <ul className="social-icons is-flex">
+              <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faFacebook} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
             </ul>
           </div>
         </div>
       </div>
       <div className="footer-bottom">
         <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <p>&copy; 2024 Your Company. All Rights Reserved.</p>
-            </div>
-          </div>
+          <p>&copy; {new Date().getFullYear()} Your Company. All Rights Reserved.</p>
         </div>
       </div>
     </footer>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bulma/css/bulma.min.css';
 
-const App = () => (
-  <div style={{ backgroundImage: 'url(client/background.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
-    <div className="container">
-      <h1>Welcome to My App</h1>
-      <p>This is a paragraph.</p>
+const App = () => {
+  const [selectedServices, setSelectedServices] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const services = ['Service 1', 'Service 2', 'Service 3', 'Service 4'];
+
+  const handleServiceSelect = (service) => {
+    if (selectedServices.includes(service)) {
+      setSelectedServices(selectedServices.filter((s) => s !== service));
+    } else {
+      setSelectedServices([...selectedServices, service]);
+    }
+  };
+
+  const addToCart = () => {
+    setCart([...cart, ...selectedServices]);
+    setSelectedServices([]);
+  };
+
+  const removeFromCart = (service) => {
+    setCart(cart.filter((item) => item !== service));
+  };
+
+  return (
+    <div className="hero is-fullheight is-bold has-background-primary" style={{ backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Welcome to My App</h1>
+          <p className="subtitle">Select the services you're interested in:</p>
+          <div className="columns is-multiline">
+            {services.map((service) => (
+              <div key={service} className="column is-one-quarter">
+                <label className="checkbox">
+                  <input type="checkbox" checked={selectedServices.includes(service)} onChange={() => handleServiceSelect(service)} />
+                  {service}
+                </label>
+              </div>
+            ))}
+          </div>
+          <button className="button is-primary" onClick={addToCart}>Add to Cart</button>
+        </div>
+      </div>
+      {cart.length > 0 && (
+        <div className="hero-foot">
+          <nav className="navbar is-light">
+            <div className="container">
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <button className="button is-primary" onClick={() => setCart([])}>Clear Cart</button>
+                    <button className="button is-primary">Checkout ({cart.length} items)</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+          <div className="container">
+            <h2 className="title is-4">Items in Cart:</h2>
+            <ul>
+              {cart.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
