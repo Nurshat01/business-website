d867de3bf34e24b2aa4848f9a3b56ddb9af2f326
Adam <nuradam3033@gmail.com>
2024-03-16 18:26:08 -0500

    rollback

--- server/server.js
+++ server/server.js
@@ -1,36 +1,44 @@
-// server.js
-
 const express = require('express');
-const { ApolloServer } = require('@apollo/server-express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
-// Apply Apollo middleware to Express
-server.applyMiddleware({ app });
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
-// Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
-}
+};
 
-// Start server
-db.once('open', () => {
-  app.listen({ port: process.env.PORT || 3001 }, () => {
-    console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 3001}${server.graphqlPath}`);
-  });
-});
+startApolloServer();
