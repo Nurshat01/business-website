3bcb7fced071d20ab7197541d1ff346910dc1fe4
Adam <nuradam3033@gmail.com>
2024-03-16 18:15:52 -0500

    fixed dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,10 @@
 import { defineConfig } from 'vite';
-// import path from 'path';
-
-// export default defineConfig({
-//   root: path.resolve(__dirname, '..'), 
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:3001', 
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, '..'), 
-//     },
-//   },
-// });
-
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
     port: 3000,
     open: true,
     proxy: {
@@ -33,6 +13,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-    }
-  }
-})
+    },
+  },
+});
